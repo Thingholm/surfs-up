77d870ac961abdf8ae527dc63795309a14fb4b64
Thingholm <rasmusthingholm@gmail.com>
2024-08-23 11:30:29 +0200

    about page

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+loadHeader();
+
 const interSectionObserverOptions = {
     root: document.querySelector("body"),
     rootMargin: "0px",
     threshold: 0.2
 }
 
-const handleIntersect = (entries, observer) => {
+function handleIntersect(entries, observer) {
     entries.forEach(entry => {
         if (entry.isIntersecting){
             entry.target.classList.add("show")
@@ -15,7 +17,15 @@ const handleIntersect = (entries, observer) => {
 
 const observer = new IntersectionObserver(handleIntersect, interSectionObserverOptions)
 
+function loadHeader() {
+    fetch('components/header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading header:', error));
+}
+
 Array.from(document.getElementsByClassName("observe")).forEach(target => {
     observer.observe(target);
 });
-
