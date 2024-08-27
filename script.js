ede6d2506dfb5ecd86f03291f9d3e10874cc33ed
Denis <deni.dj2@gmail.com>
2024-08-27 13:56:28 +0200

    Sliding-image

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-window.onload = function() {
-    const image = document.getElementById('sliding-image');
-    console.log('JavaScript is running'); // Debugging check
+// window.onload = function() {
+//     const image = document.getElementById("sliding-image");
+//     console.log('JavaScript is running'); // Debugging check
 
-    setTimeout(() => {
-        image.style.left = '0'; // Slide the image into view
-        console.log('Image sliding in'); // Debugging check
-    }, 500); // Adjust the delay (in milliseconds) as needed
-};
+//     setTimeout(() => {
+//         image.style.left = '0'; // Slide the image into view
+//         console.log('Image sliding in'); // Debugging check
+//     }, 500); // Adjust the delay (in milliseconds) as needed
+// };
+
+
+window.onload = function() {
+    document.getElementById('sliding-image').classList.add("show");
+    console.log("t")
+
+}
+
+
