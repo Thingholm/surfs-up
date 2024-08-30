d48da873a51c41aca49bf775d1f813fe641b145d
Denis <deni.dj2@gmail.com>
2024-08-30 13:01:08 +0200

    Product repository created

---
+++ SurfsUpWebApp/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace SurfsUpWebApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Thickness { get; set; }
+        public double Volume { get; set; }
+        public string Type { get; set; }
+        public double Price { get; set; }
+        public List<string> Equipment { get; set; }
+
+    }
+}
+        
