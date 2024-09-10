92c78c2135ea1667c09d6fb7ef2c786814736557
Gustav Nielsen <gustavnielsen1234@gmail.com>
2024-09-10 12:32:13 +0200

    Index filer og controller til Checkout

---
+++ SurfsUpWebApp/Controllers/CheckoutController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SurfsUpWebApp.Controllers
+{
+    public class CheckoutController : Controller 
+    {
+        public IActionResult CustomerInformation()
+        {
+            return View();
+        }
+        public IActionResult PaymentInformation()
+        {
+            return View();
+        }
+        public IActionResult OrderConfirmation()
+        {
+            return View();
+        }
+    }
+}
