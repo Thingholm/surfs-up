ecd4d2d703afc7892eae81821965a4dbb449f067
Ung D <deni.dj2@gmail.com>
2024-09-23 12:45:15 +0200

    Login Form created

---
+++ SurfsUpWebApp/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SurfsUpWebApp.Controllers
+{
+    [Route("login")]
+    public class LoginController : Controller
+    {
+        public IActionResult Login() 
+        {
+            return View();
+        }
+         /*public IActionResult Register()
+         {
+             return View();
+         }*/
+    }
+}
